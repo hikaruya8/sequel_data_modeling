a86ef05c5736142f7e261f7ed800c3a50ef59a33
山田暉 <hikaruy0805@gmail.com>
2018-10-16 12:32:32 +0900

    変数に格納

--- report_2.py
+++ report_2.py
@@ -11,15 +11,17 @@ class factor_analysis:
     x3 = (self.b0 + self.b3, self.b1)
     x4 = (self.b0 + self.b2, self.b1 + self.b2)
     x5 = (self.b0 + self.b2, self.b0 + self.b1 + self.b2)
-    print('x1:{} , x2:{} ,x3:{} ,x4:{} , x5:{}'.format(x1, x2, x3, x4, x5))
+    return x1, x2, x3, x4, x5
 
   def update_u(self):
-    
+    u = (u1, u2)
+
 
 # a,b = map(int,input().split())
 print('b3, b2, b1, b0 の順番に半角スペースを空けて入力してください')
 b3, b2 , b1, b0 = map(int, input().split())
 myinstance = factor_analysis(b3, b2, b1, b0)
-myinstance.calc_x()
+x1, x2, x3, x4, x5 = myinstance.calc_x()
+print('x1:{} , x2:{} ,x3:{} ,x4:{} , x5:{}'.format(x1, x2, x3, x4, x5))
 
 
