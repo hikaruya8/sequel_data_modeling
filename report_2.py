bafa05101a0a1e5e207ef601854a4d7de786c0cd
山田暉 <hikaruy0805@gmail.com>
2018-10-16 12:26:50 +0900

    初期値計算OK

---
+++ report_2.py
@@ -0,0 +1,25 @@
+class factor_analysis:
+  def __init__(self, b3, b2, b1, b0):
+    self.b0 = b0
+    self.b1 = b1
+    self.b2 = b2
+    self.b3 = b3
+
+  def calc_x(self):
+    x1 = (self.b2, self.b3)
+    x2 = (self.b1 + self.b3, self.b0 + self.b2)
+    x3 = (self.b0 + self.b3, self.b1)
+    x4 = (self.b0 + self.b2, self.b1 + self.b2)
+    x5 = (self.b0 + self.b2, self.b0 + self.b1 + self.b2)
+    print('x1:{} , x2:{} ,x3:{} ,x4:{} , x5:{}'.format(x1, x2, x3, x4, x5))
+
+  def update_u(self):
+    
+
+# a,b = map(int,input().split())
+print('b3, b2, b1, b0 の順番に半角スペースを空けて入力してください')
+b3, b2 , b1, b0 = map(int, input().split())
+myinstance = factor_analysis(b3, b2, b1, b0)
+myinstance.calc_x()
+
+
