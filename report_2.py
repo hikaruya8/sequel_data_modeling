ed77677f4bb53b424b6959cc2d7a700b8c2cbb13
山田暉 <hikaruy0805@gmail.com>
2018-10-16 12:43:40 +0900

    μ出すのはできた　関数化が必要

--- report_2.py
+++ report_2.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class factor_analysis:
   def __init__(self, b3, b2, b1, b0):
     self.b0 = b0
@@ -13,15 +15,23 @@ class factor_analysis:
     x5 = (self.b0 + self.b2, self.b0 + self.b1 + self.b2)
     return x1, x2, x3, x4, x5
 
-  def update_u(self):
-    u = (u1, u2)
+  def update_u(self, u):
+    u = x1 + x2 + x3 + x4 + x5
 
 
-# a,b = map(int,input().split())
+
 print('b3, b2, b1, b0 の順番に半角スペースを空けて入力してください')
 b3, b2 , b1, b0 = map(int, input().split())
 myinstance = factor_analysis(b3, b2, b1, b0)
 x1, x2, x3, x4, x5 = myinstance.calc_x()
 print('x1:{} , x2:{} ,x3:{} ,x4:{} , x5:{}'.format(x1, x2, x3, x4, x5))
+
+
+list = np.array([x1, x2, x3, x4, x5])
+sum = 0
+for i in list:
+  sum += i
+
+print(sum/5)
 
 
